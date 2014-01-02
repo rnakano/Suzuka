95f2c4872f5abe78165207e7ca13bd9fd5f88046
NAKANO Ryusuke <rsk.nakano@gmail.com>
2014-01-02 15:16:27 +0900

    add server.d

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 ARCH ?= $(shell getconf LONG_BIT)
 DMD ?= dmd
-NAMES = http main
+NAMES = main http server
 FILES = $(addsuffix .d, $(NAMES))
 SRCS  = $(addprefix src/, $(FILES))
 DFLAGS = -Isrc -m$(ARCH) -w -d -property -O -release -nofloat -inline
@@ -9,5 +9,5 @@ main: $(SRCS)
 	$(DMD) $(DFLAGS) -unittest -ofmain $(SRCS)
 
 test: main
-	./main
+	./main test
 
--- src/main.d
+++ src/main.d
@@ -1,6 +1,27 @@
-import http;
+import std.socket, std.stdio;
+import http, server;
 
 void main(string[] args)
 {
-  
+  if(args.length == 2 && args[1] == "test") return;
+  Server server = new Server(8080);
+  HttpRequestParser parser = new HttpRequestParser();
+  server.setRequestHandler( (Socket sock) {
+      char[256] buff;
+      auto read = sock.receive(buff);
+      if(Socket.ERROR == read) {
+        writeln("Connection error.");
+        sock.close();
+        return;
+      }
+      HttpRequestHeader header = parser.parse(buff[0 .. read].idup);
+      if(header is null) {
+        writeln("Parse error.");
+        sock.close();
+        return;
+      }
+      writeln("Path: " ~ header.path() ~ " requested.");
+      sock.close();
+  });
+  server.start();
 }
