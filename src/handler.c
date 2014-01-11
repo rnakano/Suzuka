a048a9018060b2dc5db3677d8f1c75c24f8e75c7
NAKANO Ryusuke <rsk.nakano@gmail.com>
2014-01-11 17:07:28 +0900

    apply path validator

--- src/handler.c
+++ src/handler.c
@@ -17,23 +17,27 @@ void sk_handler_simple_send(int sockfd)
 {
   char buff[256];
   FILE* sock_fp = fdopen(sockfd, "r+");
+  sk_http_header* header = NULL;
   
   if(fgets(buff, 256, sock_fp) == NULL){
     sk_log_debug("connection closed");
-    return;
+    goto end_handler;
   }
 
-  sk_http_header* header = sk_http_parse_header(buff);
+  header = sk_http_parse_header(buff);
   if(header == NULL) {
     sk_log_debug("parse error.");
-    fclose(sock_fp);
-    close(sockfd);
-    return;
+    goto end_handler;
   }
   sk_log_debug(header->path);
+  if(! sk_file_validate_path(header->path)) {
+    sk_sock_send_404(sock_fp);
+    goto end_handler;
+  }
   sk_handler_send_file(sock_fp, header->path);
 
-  sk_http_header_free(header);
+end_handler:
+  if(header) sk_http_header_free(header);
   fclose(sock_fp);
   close(sockfd);
 }
